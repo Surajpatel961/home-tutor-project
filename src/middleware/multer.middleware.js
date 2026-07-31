95726570d2d520f32997f72deaa93afdb9ec2767
Surajpatel961 <gurjarsuraj961@gmail.com>
2026-08-01 02:56:37 +0530

    chore: remove node_modules from version control

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,25 @@
+import { application } from "express";
+import multer from "multer"
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req , file , cb) => {
+
+    if(file.mimetype === application/pdf){
+
+        cb(null , true);
+
+    }else{
+        cb( errro , false)
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits:{
+        fileSize:10 * 1024 * 1024
+    }
+});
+
+export default upload;
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -2,10 +2,11 @@ import express from "express"
 import roleMiddleware from "../middleware/role.middleware.js";
 import { createNote , deleteNote, getAllNotes, getNote, updateNote } from "../controllers/note.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
+import upload from "../middleware/multer.middleware.js";
 
 const router = express.Router();
 
-router.post("/create-note", authMiddleware ,roleMiddleware("faculty") , createNote)
+router.post("/create-note", authMiddleware ,roleMiddleware("faculty"), upload.single("pdf"), createNote)
 
 router.get("/" , getAllNotes)
 
